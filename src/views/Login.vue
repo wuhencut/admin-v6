574e8ca91e8afa132ebdf533ad49e317471f9233
stone <stone_rivers@163.com>
2021-04-29 20:05:44 +0800

    项目搭建，setup方法

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="page"></div>
+</template>
+<script>
+import { onMounted } from '@vue/runtime-core'
+export default {
+  setup() {
+    console.log("setup")
+    onMounted(() => {
+      console.log("render")
+    })
+  },
+  created() {
+    console.log("created")
+  },
+  mounted() {
+    console.log("mounted")
+  }
+}
+</script>
